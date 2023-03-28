50eac8d44d0f669569b203311b1c438c99925e8b
nebulahh <94326584+nebulahh@users.noreply.github.com>
2023-03-28 22:38:55 +0100

    Create hashset.py

---
+++ hashset.py
@@ -0,0 +1,19 @@
+class MyHashSet:
+
+  def __init__(self):
+    self.__key = []
+ 
+  def add(self, key: int) -> None:
+    if key in self.__key:
+      return
+    self.__key.append(key)
+
+  def remove(self, key: int) -> None:
+    if key not in self.__key:
+      return 
+    self.__key.remove(key)
+
+  def contains(self, key: int) -> bool:
+    if key in self.__key:
+      return True
+    return False
