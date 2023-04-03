2df16ecf139592a68da042ec0dc252adce48d7ab
nebulahh <94326584+nebulahh@users.noreply.github.com>
2023-04-03 11:28:18 +0100

    Create contains-duplicate.py

---
+++ blind75/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+  def containsDuplicate(self, nums: List[int]) -> bool:
+    unique = set()
+
+    for x in nums:
+      if x in unique:
+        return True
+      else:
+        unique.add(x)
+    return False
