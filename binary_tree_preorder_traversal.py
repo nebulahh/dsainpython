3d191206858394cc8015da98e5c77cdf8a9fb76c
nebulahh <94326584+nebulahh@users.noreply.github.com>
2023-04-16 17:25:52 -0400

    Create binary_tree_preorder_traversal.py

---
+++ binary_tree_preorder_traversal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        answer = []
+        
+        def dfs(node):
+          if not node:
+            return
+          answer.append(node.val)
+          dfs(node.left)
+          dfs(node.right)
+        dfs(root)
+        return answer
